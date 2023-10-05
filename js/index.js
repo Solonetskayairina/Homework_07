90d966117a41dad4869e99996f83467eccb6ae86
Solonetskayairina <Solonetskayairina@ukr.net>
2023-10-05 23:28:18 +0300

    change file

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
     let numOrStr = prompt('input number or string');
     console.log(numOrStr)
 
-    switch(true) {
-      case numOrStr === null:
-        console.log( 'ви скасували' );
-        break;
-      case numOrStr.trim() === '':
-        console.log( 'Empty String' );
-        break;
-      case isNaN (+numOrStr):
-        console.log( 'number is Ba_NaN' );
-        break;
-      default:
-        console.log( "OK" );
+    switch (isNaN(+numOrStr) || numOrStr)
+    {
+        case true:
+            console.log("Is Ba_NaN!")
+            break;
+        case null:
+            console.log("ви скасували");
+            break;
+        case '':
+            console.log("Empty String");
+            break;
+        default:
+            console.log("OK");
+            break;
     }
